9d53809520e4302afe8a57e185dd14fa6c1f8cb8
rafaelogic <12.12afa3l@gmail.com>
2023-05-07 09:24:18 +0800

    update api docs and login fields name

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
+
 require __DIR__.'/api/api_v1.php';
+
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('auth.login');
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum')
+    ->name('auth.logout');
