2fb8cbe5a82539766fc2b8aa4d52aadddae4c044
rafaelogic <12.12afa3l@gmail.com>
2023-05-08 16:07:54 +0800

    add: user profile route

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\AuthUserController;
 use Illuminate\Support\Facades\Route;
 
 require __DIR__.'/api/api_v1.php';
+
+Route::get('/users/auth', AuthUserController::class)
+    ->name('user.profile');
 
 Route::post('/login', [AuthController::class, 'login'])
     ->name('auth.login');
